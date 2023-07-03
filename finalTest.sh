65a90d033eb9cbca868eb213efaa177a4e595913
root <root@instance-5.asia-south1-c.c.forward-liberty-389906.internal>
2023-07-03 17:05:32 +0530

    command line argument added

--- finalTest.sh
+++ finalTest.sh
@@ -1,12 +1,11 @@
 echo "if you want to start vm then type start"
 echo "if you want to stop vm then type stop"
 
-read data
 
-function fun()
+function fun( )
 {
 
-if  [ $data = "start" ]  && [ $date +" %H-%M " = $date +" %H-%M "  ];
+if  [ $1 = "start" ]  && [ $date +" %H-%M " = $date +" %H-%M "  ];
 then
 	gcloud config set compute/zone asia-south1-c 
 	gcloud compute instances start ubuntu-2 
@@ -14,7 +13,7 @@ then
 	gcloud config set compute/zone us-west4-b
 	gcloud compute instances start instance-4
 	gcloud compute instances start instance-2
-elif [ $data = "stop" ] && [ $date +" %H-%M " = $date +" %H-%M " ];
+elif [ $1 = "stop" ] && [ $date +" %H-%M " = $date +" %H-%M " ];
 then
 	gcloud config set compute/zone asia-south1-c
 	gcloud compute instances stop ubuntu-2
@@ -30,4 +29,4 @@ fi
 
 }
 
-fun
+fun $1
